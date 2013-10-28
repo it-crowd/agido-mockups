285d754c9db0656f96b956f6183fb459e40511b9
Bernard Labno <s4237@pjwstk.edu.pl>
2013-10-28 14:22:21 +0100

    Link component performance

--- src/components/link.js
+++ src/components/link.js
@@ -1,5 +1,16 @@
 (function ()
 {
+    function updateChildren(item)
+    {
+        var textChild = item.find(".text")[0];
+        textChild.setText(item.getText());
+        //noinspection JSUnresolvedFunction
+        item.find(".line")[0].setPoints([
+            {x: 0, y: textChild.getTextHeight()},
+            {x: textChild.getWidth(), y: textChild.getTextHeight()}
+        ]);
+    }
+
     Kinetic.Link = function (config)
     {
         this.____init(config);
@@ -9,24 +20,22 @@
         {
             this.className = "Link";
             Kinetic.Group.call(this, config);
-            this.add(new Kinetic.Text(AgidoMockups.extend(config, {id: "text", x: 0, y: 0, draggable: false, fill: config.color, stroke: null})));
-            this.add(new Kinetic.Line(AgidoMockups.extend(config, {id: "line", x: 0, y: 0, draggable: false, stroke: config.color})));
+            this.add(new Kinetic.Text(AgidoMockups.extend(config, {name: "text", x: 0, y: 0, draggable: false, fill: config.color, stroke: null})));
+            this.add(new Kinetic.Line(AgidoMockups.extend(config, {name: "line", x: 0, y: 0, draggable: false, stroke: config.color})));
             //noinspection JSUnusedLocalSymbols
             this.add = function (ignore)
             {
                 throw new Error("Cannot add children to Link");
-            }
-        }, drawScene: function (canvas)
-        {
-            var textChild = this.find("#text")[0];
-            textChild.setText(this.getText());
-            //noinspection JSUnresolvedFunction
-            this.find("#line")[0].setPoints([
-                {x: 0, y: textChild.getTextHeight()},
-                {x: textChild.getWidth(), y: textChild.getTextHeight()}
-            ]);
-            return Kinetic.Container.prototype.drawScene.call(this, canvas);
-        }, toObject: function ()
+            };
+            this.on("textChange", function (event)
+            {
+                if (event.newVal != event.oldVal) {
+                    updateChildren(this);
+                }
+            });
+            updateChildren(this);
+        },
+        toObject: function ()
         {
             return Kinetic.Node.prototype.toObject.call(this);
         }
